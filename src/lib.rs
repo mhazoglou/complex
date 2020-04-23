97fd6d98abf9b09e7a427a5edaedeff3c14527dd
Michael Hazoglou <michaelhazoglou@gmail.com>
2020-04-23 02:45:59 -0400

    Implemented pretty print for complex and quaternions. Recursive print for octonions and higher.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 use std::{ops, fmt};
 use std::str::FromStr;
 use std::num::ParseFloatError;
+use std::any::type_name;
 
+fn type_of<T>(_: &T) -> &'static str {
+    type_name::<T>()
+}
 
 #[macro_export]
 macro_rules! complex{
@@ -267,7 +271,7 @@ impl<T> ops::Div<f64> for Complex<T>
         self * (1. / other) 
     }
 }
-    
+  
 impl<T> ops::Div<Complex<T>> for f64 
     where Complex<T>: Conjugate + AbsSq + 
     ops::Mul<f64, Output = Complex<T>> + ops::Div<f64, Output = Complex<T>>
@@ -277,20 +281,53 @@ impl<T> ops::Div<Complex<T>> for f64
           other.conj() * (self / other.abs_sq())
     }
 }
-/*
-impl fmt::Display for c128 {
+
+
+impl<T> fmt::Display for Complex<T> 
+    where T: fmt::Display
+{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        if self.im > 0. {
-            return write!(f, "{}+{}i", self.re, self.im);
-        } else if self.im < 0. {
-            return write!(f, "{}{}i", self.re, self.im);
+        if type_of(&self.re) == "f64" {
+            let real = self.re.to_string();
+            let imag = self.im.to_string();
+            
+            if &imag[0..1] != "-" {
+                return write!(f, "{} + {}i", &real, &imag);
+            } else {
+                return write!(f, "{} - {}i", &real, &imag[1..]);
+            }
+        } else if type_of(&self.re) == "complex::Complex<f64>" {
+            let real = self.re.to_string();
+            let imag = self.im.to_string();
+            
+            let real_split:Vec<&str> = real.split_whitespace().collect();
+            let imag_split:Vec<&str> = imag.split_whitespace().collect();
+            
+            let len = imag_split[2].len();
+            
+            if &imag_split[0][0..1] != "-" {
+                return write!(f, "{} {} {} + {}j {} {}k", 
+                              &real_split[0], &real_split[1],
+                              &real_split[2], &imag_split[0], 
+                              &imag_split[1], &imag_split[2][..(len - 1)]
+                );
+            } else {
+                return write!(f, "{} {} {} - {}j {} {}k", 
+                              &real_split[0], &real_split[1], 
+                              &real_split[2], &imag_split[0][1..], 
+                              &imag_split[1], &imag_split[2][..(len - 1)]
+                );
+            }
         } else {
-            return write!(f, "{}", self.re);
+            return write!(f, "({}, {})", &self.re, &self.im);
         }
     }
 }
 
-impl FromStr for c128 {
+
+
+/*
+impl FromStr for complex<T> {
     type Err = ParseFloatError;
     
     fn from_str(s: &str) -> Result<Self, Self::Err> {
